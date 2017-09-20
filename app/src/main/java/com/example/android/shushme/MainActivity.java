8ef732dc530be137534deacd3395a44e2fb93882
KUMA_THE_CANDY_BANDIT <KUMA_THE_CANDY_BANDIT@gmail.com>
2017-09-19 22:58:42 -0700

    1.1 Update T0X.01-Exercise-GoogleApiClient

--- app/src/main/java/com/example/android/shushme/MainActivity.java
+++ app/src/main/java/com/example/android/shushme/MainActivity.java
@@ -18,6 +18,7 @@ package com.example.android.shushme;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -43,6 +44,8 @@ import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener,GoogleApiClient.ConnectionCallbacks{
 
+    private static final String TAG = MainActivity.class.getName();
+
     // Member variables
     private PlaceListAdapter mAdapter;
     private RecyclerView mRecyclerView;
@@ -50,7 +53,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     //  GoogleApiClient
     private GoogleApiClient mGoogleAPIClient;
 
-    //String Constrant
+    //String Constant
     private static final String PERMISSIONS_STRING[] = {android.Manifest.permission.ACCESS_FINE_LOCATION};
     //Request Code
     private static final int PERMISSIONS_REQUEST_CODE = 101;
@@ -58,10 +61,11 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     // TODO (8) Implement onLocationPermissionClicked to handle the CheckBox click event
     @OnClick(R.id.enable_location)
     public void onLocationPermissionClicked(CheckBox checkbox){
-        if(!checkbox.isChecked()){
+        if(checkbox.isChecked()){
             ActivityCompat.requestPermissions(this,
                     PERMISSIONS_STRING, PERMISSIONS_REQUEST_CODE);
         }
+        Log.d(TAG,"OnLocationPermissionClicke");
         Timber.d("Checkbox clicked");
     }
 
@@ -70,10 +74,12 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     public void onAddNewLocationClicked(Button button) {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             Timber.d("Permissions ACCESS_FINE_LOCATION Enabled");
-            Toast.makeText(this,"Permissions ACCESS_FINE_LOCATION Enabled",Toast.LENGTH_SHORT).show();
+            Log.d(TAG,"Permissions ACCESS_FINE_LOCATION Enabled");
+            Toast.makeText(this,"Permissions ACCESS_FINE_LOCATION Enabled",Toast.LENGTH_LONG).show();
         }else{
             Timber.d("Permissions ACCESS_FINE_LOCATION Disabled");
-            Toast.makeText(this,"Permissions ACCESS_FINE_LOCATION Disabled",Toast.LENGTH_SHORT).show();
+            Log.d(TAG,"Permissions ACCESS_FINE_LOCATION Disabled" );
+            Toast.makeText(this,"Permissions ACCESS_FINE_LOCATION Disabled",Toast.LENGTH_LONG).show();
         }
     }
 
@@ -93,6 +99,10 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         mAdapter = new PlaceListAdapter(this);
         mRecyclerView.setAdapter(mAdapter);
 
+        Timber.plant(new Timber.DebugTree());
+
+        ButterKnife.bind(this);
+
         // TODO (4) Create a GoogleApiClient with the LocationServices API and GEO_DATA_API
         mGoogleAPIClient = new GoogleApiClient.
                 Builder(this).
@@ -100,11 +110,8 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
                 addApi(Places.PLACE_DETECTION_API).
                 addConnectionCallbacks(this).
                 addOnConnectionFailedListener(this).
+                enableAutoManage(this,this).
                 build();
-
-        Timber.plant(new Timber.DebugTree());
-
-        ButterKnife.bind(this);
     }
 
     // TODO (5) Override onConnected, onConnectionSuspended and onConnectionFailed for GoogleApiClient
