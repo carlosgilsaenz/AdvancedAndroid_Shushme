d6e82e47b1107ddd845ff413002b47764ac9cc19
Saenz <carlos.saenz@stantec.com>
2017-09-27 17:29:04 -0700

    1.1 Update T0X.04-Exercise-Geofencing

--- app/src/main/java/com/example/android/shushme/Geofencing.java
+++ app/src/main/java/com/example/android/shushme/Geofencing.java
@@ -1,8 +1,15 @@
+package com.example.android.shushme;
+
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.nfc.Tag;
+import android.util.Log;
 
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingRequest;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.PlaceBuffer;
 
@@ -18,6 +25,8 @@ public class Geofencing {
 
     // Member variables
     private static final int DAYS_FOR_GEOFENCE = 1;
+    private static final float GEOFENCE_RADIUS_IN_METERS = 5;
+    private static final String TAG = Geofencing.class.getName();
 
     private GoogleApiClient mGoogleClient;
     private Context mContext;
@@ -39,11 +48,29 @@ public class Geofencing {
             Geofence geofence = new Geofence.Builder().
                     setRequestId(place.getId()).
                     setExpirationDuration(TimeUnit.DAYS.toMillis(DAYS_FOR_GEOFENCE)).
+                    setCircularRegion(place.getLatLng().latitude,place.getLatLng().longitude,
+                            GEOFENCE_RADIUS_IN_METERS).
                     setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER |
                             Geofence.GEOFENCE_TRANSITION_EXIT).
                     build();
             
             mGeofenceList.add(geofence);
         }
+    }
+
+    public GeofencingRequest getGeofencingRequest(){
+        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_DWELL);
+        builder.addGeofences(mGeofenceList);
+        return builder.build();
+    }
+
+    public PendingIntent getGeofencePendingIntent(){
+        if(mGeofencePendingIntent != null){return mGeofencePendingIntent;}
+
+        Intent intent = new Intent(mContext,Geofencing.class);
+        mGeofencePendingIntent = PendingIntent.getBroadcast(mContext,0,intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        return mGeofencePendingIntent;
     }
 }
--- app/src/main/java/com/example/android/shushme/MainActivity.java
+++ app/src/main/java/com/example/android/shushme/MainActivity.java
@@ -98,31 +98,31 @@ public class MainActivity extends AppCompatActivity implements
                 .enableAutoManage(this, this)
                 .build();
 
-        // TODO (1) Create a Geofencing class with a Context and GoogleApiClient constructor that
+        // TODO (1) Create a com.example.android.shushme.Geofencing class with a Context and GoogleApiClient constructor that
         // initializes a private member ArrayList of Geofences called mGeofenceList
 
-        // TODO (2) Inside Geofencing, implement a public method called updateGeofencesList that
+        // TODO (2) Inside com.example.android.shushme.Geofencing, implement a public method called updateGeofencesList that
         // given a PlaceBuffer will create a Geofence object for each Place using Geofence.Builder
         // and add that Geofence to mGeofenceList
 
-        // TODO (3) Inside Geofencing, implement a private helper method called getGeofencingRequest that
+        // TODO (3) Inside com.example.android.shushme.Geofencing, implement a private helper method called getGeofencingRequest that
         // uses GeofencingRequest.Builder to return a GeofencingRequest object from the Geofence list
 
         // TODO (4) Create a GeofenceBroadcastReceiver class that extends BroadcastReceiver and override
         // onReceive() to simply log a message when called. Don't forget to add a receiver tag in the Manifest
 
-        // TODO (5) Inside Geofencing, implement a private helper method called getGeofencePendingIntent that
+        // TODO (5) Inside com.example.android.shushme.Geofencing, implement a private helper method called getGeofencePendingIntent that
         // returns a PendingIntent for the GeofenceBroadcastReceiver class
 
-        // TODO (6) Inside Geofencing, implement a public method called registerAllGeofences that
+        // TODO (6) Inside com.example.android.shushme.Geofencing, implement a public method called registerAllGeofences that
         // registers the GeofencingRequest by calling LocationServices.GeofencingApi.addGeofences
         // using the helper functions getGeofencingRequest() and getGeofencePendingIntent()
 
-        // TODO (7) Inside Geofencing, implement a public method called unRegisterAllGeofences that
+        // TODO (7) Inside com.example.android.shushme.Geofencing, implement a public method called unRegisterAllGeofences that
         // unregisters all geofences by calling LocationServices.GeofencingApi.removeGeofences
         // using the helper function getGeofencePendingIntent()
 
-        // TODO (8) Create a new instance of Geofencing using "this" as the context and mClient as the client
+        // TODO (8) Create a new instance of com.example.android.shushme.Geofencing using "this" as the context and mClient as the client
 
     }
 
@@ -227,6 +227,8 @@ public class MainActivity extends AppCompatActivity implements
             // Extract the place information from the API
             String placeName = place.getName().toString();
             String placeAddress = place.getAddress().toString();
+            String placeLanLong = place.getLatLng().toString();
+            Toast.makeText(this,placeLanLong,Toast.LENGTH_LONG).show();
             String placeID = place.getId();
 
             // Insert a new place into DB
